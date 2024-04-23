0a4159f5dddf4b94991ba03ad9996a48fd5ce5a4
kunlinl <scu2018lkl@gmail.com>
2024-04-23 16:33:19 +0800

    chore: supply options for TimerToken

--- apps/angular/injection-token/src/app/data.ts
+++ apps/angular/injection-token/src/app/data.ts
@@ -2,4 +2,7 @@ import { InjectionToken } from '@angular/core';
 
 export const DEFAULT_TIMER = 1000;
 
-export const TimerToken = new InjectionToken<number>('timer_value');
+export const TimerToken = new InjectionToken<number>('timer_value', {
+  providedIn: 'root',
+  factory: () => DEFAULT_TIMER,
+});
